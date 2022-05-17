22b46c63d3d9be6b34d8695de46dded11859f7e1
debenedettig <debenedettig@mantis.mathcs.carleton.edu>
2022-05-17 16:09:05 -0500

    started

--- interpreter.c
+++ interpreter.c
@@ -10,10 +10,48 @@
 #include <string.h>
 #include <ctype.h>
 
+Value *lookUpSymbol(Value *tree, Frame *frame){
+   while (frame != NULL){
+      Value *bindings = frame->bindings;
+      while (bindings->type != NULL_TYPE){
+         *binding = car(bindings);
+         if (car(tree) == binding->s){
+            return cdr(binding);
+         }
+         bindings = cdr(bindings);
+      }
+      frame = frame->parent;
+   }
+   evaluationError("evalution error");
+   return NULL;
+}
+
+Value *createBindings(Frame *frame, Value *bindings){
+   //set all correct bindings to symbols
+   return 0;
+}
+
+Value *evalIf(Value *args, Frame *frame){
+   if (length(args) != 3){
+      evaluationError("evalution error");
+   }
+   if ((eval(car(args), frame))->type != BOOL_TYPE){
+      evaluationError("evalution error");
+   } else {
+      if (eval(car(args))->i == 1){
+      return eval(car(cdr(args)));
+      } else {
+         return eval( car(cdr(cdr(args))));
+      }
+   }
+   return NULL;
+}
+
 //calls eval for each top-level S-expression in the program. 
 //You should print out any necessary results before moving on to the next S-expression.
 void interpret(Value *tree){
-
+   //for s-expression in program:
+      eval(tree, frame)
 }
 
 Value *eval(Value *tree, Frame *frame) {
@@ -22,8 +60,18 @@ Value *eval(Value *tree, Frame *frame) {
         ...
         break;
      }
-     case ......: {
-        ...
+     case STR_TYPE: {
+        if (tree->s == "let"){
+           //construct a frame
+           *newFrame = makeNull();
+           newFrame->parent = frame;
+           for (i=0 )
+
+           //This frame should be passed in when calling eval on the body 
+           //of the let expression. The frame is used to resolve (find the value of) 
+           //each variable encountered during evaluation. When eval tries to 
+           //evaluate x, it looks in the current frame to find a value for x.
+        }
         break;
      }  
      case SYMBOL_TYPE: {
